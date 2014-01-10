c95b96ea81f846203e55994d9b3b58844d0400fa
A. Gordon <agordon@wi.mit.edu>
2014-01-10 18:24:15 -0500

    HomeBrew Formula: ugly hack to extract GIT version.

--- gordon_bin_scripts.rb
+++ gordon_bin_scripts.rb
@@ -10,9 +10,31 @@ class GordonBinScripts < Formula
   end
 
   def install
-      system 'sh', './bootstrap.sh'
-      system './configure', "--prefix=#{prefix}"
-      system 'make'
-      system 'make', 'install'
+
+    if build.head?
+      # Ugly hack: The build system extracts the version from either ".git" (if cloned)
+      #            or from ".tarball-version" (if from a proper dist tarball).
+      #            But here, HomeBrew clones (shallowly) first, then extract to a separate directory,
+      #            So there's neither ".git" nor ".tarball-version".
+      #
+      #            Here, we go back to the GIT directory, 'unshallow' the repository
+      #            (unshallow requires git version 1.8.3 or later), then extract the version.
+      #            If 'unshallowing' failed, fallback to the SHA1 of the current version.
+      ohai "Trying to extract version from Git Repository"
+      git_repo = "#{HOMEBREW_CACHE}/#{name}--git"
+      prog_version = `(cd #{git_repo} ; git fetch --unshallow ; ./config/git-version-gen v )`
+      prog_version.chomp!
+      if prog_version == "UNKNOWN"
+        git_version = `(cd #{git_repo} ; git describe --always )`
+        git_version.chomp!
+        prog_version = "#{git_version}-HomeBrew-Git"
+      end
+      ohai "Detected Git version = #{prog_version}"
+      system "echo '#{prog_version}' > .tarball-version"
+    end
+    system 'sh', './bootstrap.sh'
+    system './configure', "--prefix=#{prefix}"
+    system 'make'
+    system 'make', 'install'
   end
 end
